8d6b6af549e26e5e34d2019a7b4ba1068147f732
kgytis <kaziukon.gytis@gmail.com>
2022-03-22 09:46:25 +0200

    Atlikta 8 užduotis

--- Atsiskaitymas/JS atsiskaitymas/Task 8/script.js
+++ Atsiskaitymas/JS atsiskaitymas/Task 8/script.js
@@ -5,3 +5,28 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    sum = (number1, number2) => {
+        console.log(`Skaičius 1 + skaičius 2 = ${number1+number2}`)
+    }
+    subtraction = (number1, number2) => {
+        console.log(`Skaičius 1 - skaičius 2 = ${number1 - number2}`)
+        console.log(`Skaičius 2 - skaičius 1 = ${number2 - number1}`)
+
+    }
+    multiplication = (number1, number2) => {
+        console.log(`Skaičius 1 * skaičius 2 = ${number1*number2}`)
+    }
+    division = (number1, number2) => {
+        console.log(`Skaičius 1 / skaičius 2 = ${number1 / number2}`)
+        console.log(`Skaičius 2 / skaičius 1 = ${number2 / number1}`)
+    }
+}
+
+let veiksmai = new Calculator
+
+veiksmai.sum(10,5);
+veiksmai.subtraction(10,5)
+veiksmai.multiplication(10,5)
+veiksmai.division(10,5)
