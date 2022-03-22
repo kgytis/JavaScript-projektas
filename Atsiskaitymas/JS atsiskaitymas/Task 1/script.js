e8dced58b775fdbfe33fccd236db9f7fcf460007
kgytis <kaziukon.gytis@gmail.com>
2022-03-22 10:24:47 +0200

    Atlikta 1 užduotis

--- Atsiskaitymas/JS atsiskaitymas/Task 1/script.js
+++ Atsiskaitymas/JS atsiskaitymas/Task 1/script.js
@@ -8,3 +8,42 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+
+const forma = document.querySelector("form");
+const output = document.querySelector("#output");
+
+
+forma.addEventListener("submit", e => {
+    e.preventDefault();
+    output.innerHTML = ``
+    let mase = e.target.elements.search.value
+    const bendrasDiv = document.createElement("div")
+    bendrasDiv.setAttribute("class", "bendrasDiv")
+
+    const kilogramai = document.createElement("h2")
+    const kilogramaiText = document.createTextNode(`Masė kilogramais - ${mase} kg`)
+
+    const gramai = document.createElement("h2")
+    const gramaiText = document.createTextNode(`Masė gramais - ${mase / 0.0010000} g`)
+
+    const svarai = document.createElement("h2")
+    const svaraiText = document.createTextNode(`Masė svarais - ${mase * 2.2046} lb`)
+
+    const uncijos = document.createElement("h2")
+    const uncijosText = document.createTextNode(`Masė uncijomis - ${mase * 35.274} oz`)
+
+    kilogramai.append(kilogramaiText)
+    gramai.append(gramaiText)
+    svarai.append(svaraiText)
+    uncijos.append(uncijosText)
+
+    bendrasDiv.append(kilogramai)
+    bendrasDiv.append(gramai)
+    bendrasDiv.append(svarai)
+    bendrasDiv.append(uncijos)
+
+    output.append(bendrasDiv)
+
+    e.target.elements.search.value = null;
+})
