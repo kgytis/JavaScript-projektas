3cc76db294e553b25634d67710a2b2a83177759e
kgytis <kaziukon.gytis@gmail.com>
2022-03-22 09:52:38 +0200

    Atlikta 9 užduotis

--- Atsiskaitymas/JS atsiskaitymas/Task 9/script.js
+++ Atsiskaitymas/JS atsiskaitymas/Task 9/script.js
@@ -6,3 +6,23 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+
+class Movie {
+    constructor(
+        title,
+        director,
+        budget
+    ) {
+      this.title = title;
+      this.director = director;
+      this.budget = budget;  
+      this.wasExpensive()
+    }
+    wasExpensive = () => {
+        let palyginimas = this.budget > 100000000 ? true : false
+        console.log(palyginimas)
+    }
+}
+
+let filmas = new Movie("Spider-Man: No Way Home", "Jon Watts", 2000000000)
