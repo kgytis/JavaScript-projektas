8a4764d71b0cedddd9857407cb633777fb7499df
kgytis <kaziukon.gytis@gmail.com>
2022-03-22 09:14:12 +0200

    Atlikta 4 užduotis

--- Atsiskaitymas/JS atsiskaitymas/Task 4/script.js
+++ Atsiskaitymas/JS atsiskaitymas/Task 4/script.js
@@ -10,3 +10,40 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+const output = document.querySelector("#output")
+
+fetch(ENDPOINT)
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    atvaizdavimas(data)
+})
+
+atvaizdavimas = (info) => {
+    info.forEach(e => {
+        let naujasDiv = document.createElement("div");
+        let brand = document.createElement("h2");
+        let brandText = document.createTextNode(e.brand)
+
+        brand.append(brandText)
+        naujasDiv.append(brand)
+        naujasDiv.append(modeliai(e.models))
+        output.append(naujasDiv)
+        output.style.display = "grid"
+        output.style.gridTemplateColumns = "repeat(10, 1fr)"
+        output.style.gridTemplateRows = "repeat(4, 1fr)"
+    })
+}
+
+modeliai = (info) => {
+    let models = document.createElement("ul")
+    info.forEach(e => {
+        let listItem = document.createElement("li")
+        let listItemText = document.createTextNode(`${e}`)
+        //console.log(listItemText)
+        listItem.append(listItemText)
+        models.append(listItem)
+        //console.log(listItem)
+    })
+    return models
+}
